ffcd2a8967226551c12171ed65864ff30c787e43
Zabolotnyi_Artem <zaaabik@gmail.com>
2023-10-27 21:06:14 +0400

    added timeouts

--- api/ChatBotAPI.py
+++ api/ChatBotAPI.py
@@ -1,37 +1,64 @@
 import json
+from typing import Any
+
 import requests
 
-HEADERS = {'Accept': 'application/json'}
+HEADERS = {"Accept": "application/json"}
 
 
 class ChatBotAPI:
+    """Class using to interact with chatbot API."""
+
     def __init__(self, api_path: str):
         self.api_path = api_path
+        self.timeout = 60 * 3
 
-    def get_user(self, telegram_user_id: str):
-        path = f'{self.api_path}/dialog/{telegram_user_id}'
-        answer = requests.get(
-            path, headers=HEADERS
-        )
+    def get_user(self, telegram_user_id: str) -> Any:
+        """Return user entity :param telegram_user_id:
+
+        :ctx update:
+        """
+        path = f"{self.api_path}/dialog/{telegram_user_id}"
+        answer = requests.get(path, headers=HEADERS, timeout=self.timeout)
         return json.dumps(answer.json(), ensure_ascii=False)
 
-    def add_message(self, telegram_user_id: str, text: str):
-        path = f'{self.api_path}/dialog/{telegram_user_id}?text={text}'
+    def add_message(self, telegram_user_id: str, username: str, text: str):
+        """Clear history command handler.
+
+        Send to chat api request to remove user message history
+        Args:
+            telegram_user_id: id of user in telegram
+            username: telegram username
+            text: user message
+        """
+        path = f"{self.api_path}/dialog/{telegram_user_id}"
         answer = requests.patch(
-            path, headers=HEADERS
+            path,
+            headers=HEADERS,
+            timeout=self.timeout,
+            json={
+                "username": username,
+                "text": text,
+            },
         )
         return answer.json()
 
     def remove_user(self, telegram_user_id: str):
-        path = f'{self.api_path}/dialog/{telegram_user_id}'
-        answer = requests.delete(
-            path, headers=HEADERS
-        )
+        """
+        Remove user from database by telegram id
+        Args:
+            telegram_user_id: id of user in telegram
+        """
+        path = f"{self.api_path}/dialog/{telegram_user_id}"
+        answer = requests.delete(path, headers=HEADERS, timeout=self.timeout)
         return json.dumps(answer.json(), ensure_ascii=False)
 
     def clear_history(self, telegram_user_id: str):
-        path = f'{self.api_path}/dialog/{telegram_user_id}/context'
-        answer = requests.delete(
-            path, headers=HEADERS
-        )
+        """Clear user message history.
+
+        Args:
+            telegram_user_id: id of user in telegram
+        """
+        path = f"{self.api_path}/dialog/{telegram_user_id}/context"
+        answer = requests.delete(path, headers=HEADERS, timeout=self.timeout)
         return json.dumps(answer.json(), ensure_ascii=False)
--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, CommandHandler
-from api.ChatBotAPI import ChatBotAPI
+#  type: ignore
+
 import os
 
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+)
 
-api_path = os.environ['CHAT_API_ADDRESS']
-tg_bot_token = os.environ['TG_BOT_TOKEN']
+from api.ChatBotAPI import ChatBotAPI
+
+api_path = os.environ["CHAT_API_ADDRESS"]
+tg_bot_token = os.environ["TG_BOT_TOKEN"]
 
 
 class TgBot:
@@ -14,47 +22,69 @@ class TgBot:
         self.chat_bot = chat_bot
 
     async def get_whole_user_handler(self, update: Update, ctx: ContextTypes.DEFAULT_TYPE) -> None:
+        # pylint: disable=unused-argument
+        """Return whole user handler
+        Args:
+            update: bot update class
+            ctx: bot context
+        """
         telegram_user_id: str = str(update.effective_user.id)
         response = self.chat_bot.get_user(telegram_user_id)
         await update.message.reply_text(response)
 
     async def remove_user_handler(self, update: Update, ctx: ContextTypes.DEFAULT_TYPE) -> None:
+        # pylint: disable=unused-argument
+        """Handler for removing user whole entity via chatbot service.
+
+        Args:
+            update: bot update class
+            ctx: bot context
+        """
         telegram_user_id: str = str(update.effective_user.id)
         response = self.chat_bot.remove_user(telegram_user_id)
         await update.message.reply_text(response)
 
     async def message_handler(self, update: Update, ctx: ContextTypes.DEFAULT_TYPE) -> None:
+        """Any message handler, send request to chatbot to generate answer using LLM
+        update:
+
+        Args:
+            update: bot update class
+            ctx: bot context
+        """
         telegram_user_id: str = str(update.effective_user.id)
+        user_name = str(update.effective_user.username)
         text = str(update.message.text)
-        response = self.chat_bot.add_message(telegram_user_id, text)
-        text_answer = response['bot_answer']['context']
+        response = self.chat_bot.add_message(telegram_user_id, user_name, text)
+        text_answer = response["bot_answer"]["context"]
         await update.message.reply_text(text_answer)
-        await ctx.bot.send_chat_action(chat_id=ctx._chat_id, action='typing')
+        await ctx.bot.send_chat_action(chat_id=ctx._chat_id, action="typing")
 
     async def clear_history_handler(self, update: Update, ctx: ContextTypes.DEFAULT_TYPE) -> None:
+        # pylint: disable=unused-argument
+        """Clear history command handler.
+
+        Send to chat api request to remove user message history
+        Args:
+            update: bot update class
+            ctx: bot context
+        """
         telegram_user_id: str = str(update.effective_user.id)
         response = self.chat_bot.clear_history(telegram_user_id)
         await update.message.reply_text(response)
 
     def run(self):
-        self.app.add_handler(CommandHandler(
-            command='get',
-            callback=self.get_whole_user_handler
-        ))
+        """Run bot."""
+        self.app.add_handler(CommandHandler(command="get", callback=self.get_whole_user_handler))
         # self.app.add_handler(CommandHandler(
         #     command='remove',
         #     callback=self.remove_user_handler
         # ))
-        self.app.add_handler(CommandHandler(
-            command='clear',
-            callback=self.clear_history_handler
-        ))
-        self.app.add_handler(MessageHandler(
-            filters=None,
-            callback=self.message_handler
-        ))
+        self.app.add_handler(CommandHandler(command="clear", callback=self.clear_history_handler))
+        self.app.add_handler(MessageHandler(filters=None, callback=self.message_handler))
         self.app.run_polling()
 
-chat_bot = ChatBotAPI(api_path=api_path)
-bot = TgBot(tg_bot_token, chat_bot)
+
+chat_bot_api = ChatBotAPI(api_path=api_path)
+bot = TgBot(tg_bot_token, chat_bot_api)
 bot.run()
